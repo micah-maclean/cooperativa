3425e0ddafd28753b10ac276c43f7b13f01bcc4a
Michaela Maclean <72842715+mickey-maclean@users.noreply.github.com>
2026-07-16 08:13:52 -0300

    chore: setup inicial do projeto Spring Boot com dependências e Docker

---
+++ src/test/java/com/micahmaclean/cooperativa/CooperativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micahmaclean.cooperativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
